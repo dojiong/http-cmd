48799269dfcbdab309e535cfe717b30ef79087dd
lolynx <lodevil@live.cn>
2014-10-17 19:10:21 +0800

    client opt parse

--- httpcmd/cli.py
+++ httpcmd/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import argparse
 import requests
 import json
 import sys
@@ -8,31 +9,48 @@ import sys
 
 class CmdClient(object):
 
-    def __init__(self, addr):
-        self.addr = addr
-        self.url = 'http://%s/cmd/' % addr
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.url = 'http://%s:%d/cmd/' % (host, port)
 
     def request(self, cmd, **kwargv):
-        rep = requests.post(self.url + cmd, json=kwargv)
+        try:
+            rep = requests.post(self.url + cmd, json=kwargv)
+        except requests.exceptions.ConnectionError:
+            print('connect fail:(%s:%d)' % (self.host, self.port),
+                  file=sys.stderr)
+            raise SystemExit
         return rep.json()
 
 
-def partial_args():
+def partial_args(cl_argv):
     args = {}
-    for item in sys.argv[3:]:
-        parts = item.split('=')
-        if len(parts) != 2:
-            print('invalid arg: `%s`' % item, file=sys.stderr)
-        args[parts[0]] = parts[1]
+    if cl_argv is not None:
+        for item in cl_argv:
+            parts = item.split('=')
+            if len(parts) != 2:
+                print('invalid arg: `%s`' % item, file=sys.stderr)
+                raise SystemExit
+            args[parts[0]] = parts[1]
     return args
 
 
+def get_args():
+    parser = argparse.ArgumentParser('http-cmd client')
+    parser.add_argument('-i', '--host', dest='host', default='127.0.0.1',
+                        help='cmd server host')
+    parser.add_argument('-p', '--port', dest='port', type=int, default=5000,
+                        help='cmd server port')
+    parser.add_argument('cmd', help='cmd to request')
+    parser.add_argument('args', metavar='arg', nargs='*', help='args of cmd')
+    return parser.parse_args()
+
+
 def main():
-    if len(sys.argv) < 3:
-        print('usage: httpcmd ip:port cmd k=v ...', file=sys.stderr)
-        raise SystemExit
-    cli = CmdClient(sys.argv[1])
-    rep = cli.request(sys.argv[2], **partial_args())
+    args = get_args()
+    cli = CmdClient(args.host, args.port)
+    rep = cli.request(args.cmd, **partial_args(args.args))
     print(json.dumps(rep, indent=2))
 
 
